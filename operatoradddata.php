506b9a69c361a2a045c021654c8b6da7c0c5ffbf
Arun Haridas <arunharidas92@yahoo.com>
2023-05-01 22:49:12 +0530

    save application modified

--- operatoradddata.php
+++ operatoradddata.php
@@ -11,7 +11,7 @@
     <?php
         require("header.html");
         require("menu.php");
-        if(!isset($_POST)){
+        if(empty($_POST)){
             $date = date("d/m/Y");
             $visitorname="";
             $visitormobile="";
@@ -34,8 +34,39 @@
             $servicemoredetails="";
 
         }
-        else{               // get data when editing
+        else{                                // get post data
+            $date = $_POST['date'];
+            $visitorname = $_POST['name'];
+            $visitormobile = $_POST['mobile'];
+            $visiordetails = $_POST['visitorDetails'];
+            $servicefor = $_POST['service'];
+            $servicedepartment = $_POST['department'];
+            $serviceprocedures = $_POST['procedures'];
+            $listofdocuments = $_POST['documents'];
+            $feedetails = $_POST['feedetails'];
+            $source = $_POST['source'];
+            $websiteurl = $_POST['sourceMore'];
+            $servicedetails = $_POST['serviceDetails'];
+            $chargecfc = $_POST['chargeCFC'];
+            $servicegiven = isset($_POST['isServiceGiven'])? 1 : 0;
+            $district = $_SESSION['district'];
+            $office = $_SESSION['office'];
+            $filenote = "";
 
+            
+        }
+        // -----------------Save Data
+        if(isset($_POST['savedata'])){
+            require_once('config.php');
+            $sql = "INSERT INTO applications VALUES(NULL,'$date','$visitorname','$visitormobile','$visiordetails','$servicefor', '$servicedepartment', '$serviceprocedures', '$listofdocuments', '$feedetails', '$source', '$websiteurl', '$servicedetails', '$chargecfc', '$servicegiven', '$district', '$office', '$filenote');";
+            $result = mysqli_query($db,$sql) or die("Error");
+            echo '<div class="d-print-none">';
+            echo '<div class="alert alert-success" role="alert">';
+            echo 'Date Saved successfully.';
+            if($servicegiven==0){
+                echo ' <br/> <small> Complete the application process from pending menu </small> ';
+            }
+            echo '</div> </div>';
         }
     ?>
 </head>
@@ -46,7 +77,7 @@
         <table width="1000px">
             <tr>
                 <td align="center">
-                    <form method="post" action="saveapplication.php">
+                    <form method="post" >
                         <h3> Register </h3>
                         <table class="table" width="100%">
                             <tr>
@@ -157,8 +188,8 @@
                                 </td>
                                 <td>
                                     <textarea class="form-control" name="service" rows="3"
-                                        placeholder="Servie sought for" required></textarea>
-                                    <button type="button" class="btn btn-info btn-sm">Search / Import</button>
+                                        placeholder="Servie sought for"></textarea>
+                                    <button type="submit" name="getfromwiki" class="btn btn-info btn-sm">Search / Import</button>
                                 </td>
                             </tr>
                             <tr>
@@ -185,8 +216,7 @@
                                 </td>
                                 <td>
                                     <textarea class="form-control" name="procedures" rows="4"
-                                        placeholder="Procedures to be followed  for availing the service"
-                                        required></textarea>
+                                        placeholder="Procedures to be followed  for availing the service"></textarea>
                                 </td>
                             </tr>
                             <tr>
@@ -212,8 +242,7 @@
                                     Source of Information<font color="red">*</font>
                                 </td>
                                 <td>
-                                    <input class="form-control" name="source" placeholder="Source of information"
-                                        required>
+                                    <input class="form-control" name="source" placeholder="Source of information">
                                 </td>
                             </tr>
                             <tr>
@@ -299,7 +328,7 @@
                             </tr>
                         </table>
                         <br />
-                        <button type="submit" class="btn btn-success"> Submit Details </button>
+                        <button type="submit" class="btn btn-success" name="savedata"> Submit Details </button>
                     </form>
                 </td>
             </tr>
